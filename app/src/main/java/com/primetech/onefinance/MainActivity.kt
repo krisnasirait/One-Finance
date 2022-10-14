aca70b5d68fd6cab382a3d377db348ee07c71709
Krisna Sirait <krisnapande00@outlook.com>
2022-10-14 23:32:11 +0700

    working navigation

--- app/src/main/java/com/primetech/onefinance/MainActivity.kt
+++ app/src/main/java/com/primetech/onefinance/MainActivity.kt
@@ -2,10 +2,26 @@ package com.primetech.onefinance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.primetech.onefinance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navView: BottomNavigationView = binding.botNavBar
+
+        val navController  = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
     }
 }
