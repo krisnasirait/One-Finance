578f1d4f097344287f59f76460f7b69c8b12411c
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-15 23:39:18 +0700

    Add animation and implement in SplashScreenActivity.kt

--- app/src/main/java/com/primetech/onefinance/presentation/branding/SplashScreenActivity.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/SplashScreenActivity.kt
@@ -1,8 +1,12 @@
 package com.primetech.onefinance.presentation.branding
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.primetech.onefinance.MainActivity
+import com.primetech.onefinance.R
 import com.primetech.onefinance.databinding.ActivitySplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
@@ -14,5 +18,20 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        binding.ivOneAbove.startAnimation(topAnimation)
+        binding.ivOneBelow.startAnimation(bottomAnimation)
+
+        binding.ivSplashDummy.alpha = 0f
+        binding.ivSplashDummy.animate().setDuration(2700).alpha(1f).withEndAction{
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
