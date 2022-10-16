c1348ff53d41cc6c378e23eeac6774c5cbb0e9ca
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 17:29:12 +0700

    Add anim in logo landing page

--- app/src/main/java/com/primetech/onefinance/presentation/branding/LandingPageActivity.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/LandingPageActivity.kt
@@ -2,6 +2,8 @@ package com.primetech.onefinance.presentation.branding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.primetech.onefinance.R
 import com.primetech.onefinance.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class LandingPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLandingPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.landing_page_animation)
+
+        binding.ivLogoLandingUp.startAnimation(logoAnimation)
+        binding.ivLogoLandingBot.startAnimation(logoAnimation)
+
     }
 }
