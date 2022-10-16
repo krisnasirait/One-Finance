e52bbbe7d64a848d7f40096cae2449c6e4bb1227
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 15:02:39 +0700

    Make LandingPageActivity.kt

---
+++ app/src/main/java/com/primetech/onefinance/presentation/auth/LandingPageActivity.kt
@@ -0,0 +1,16 @@
+package com.primetech.onefinance.presentation.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.onefinance.databinding.ActivityLandingPageBinding
+
+class LandingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLandingPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLandingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
