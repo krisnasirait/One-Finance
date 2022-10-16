434ec477e940b9d9f8c27a4450f1f7acaa1fd089
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 18:37:50 +0700

    Make class ViewPagerAdapter.kt

---
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.primetech.onefinance.presentation.branding.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter (
+    private val items : ArrayList<Fragment>,
+    activity : AppCompatActivity
+) : FragmentStateAdapter(activity){
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return items[position]
+    }
+}
