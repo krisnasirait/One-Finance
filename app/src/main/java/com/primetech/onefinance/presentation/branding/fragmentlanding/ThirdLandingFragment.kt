5fae53e83a0a55f7f8d3ac6c2dee81f225c2ff02
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 19:05:35 +0700

    Add intent SKIP on ThirdLandingFragment.kt to MainActivity

--- app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/ThirdLandingFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/ThirdLandingFragment.kt
@@ -1,20 +1,38 @@
 package com.primetech.onefinance.presentation.branding.fragmentlanding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.primetech.onefinance.R
+import com.primetech.onefinance.MainActivity
+import com.primetech.onefinance.databinding.FragmentThirdLandingBinding
 
 class ThirdLandingFragment : Fragment() {
+
+    private lateinit var binding : FragmentThirdLandingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_landing, container, false)
+        binding = FragmentThirdLandingBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener(){
+        binding.tvSkip.setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 
 }
