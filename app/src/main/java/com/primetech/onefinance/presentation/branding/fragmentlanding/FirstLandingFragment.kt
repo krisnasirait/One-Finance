64bef3010e001a29dbc6b467ad9c70d9b8f92971
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 19:09:32 +0700

    Make FirstLandingFragment.kt and SecondLandingFragment.kt use viewBinding approach.

--- app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/FirstLandingFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/FirstLandingFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.primetech.onefinance.R
+import com.primetech.onefinance.databinding.FragmentFirstLandingBinding
 
 class FirstLandingFragment : Fragment() {
+
+    private lateinit var binding : FragmentFirstLandingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_landing, container, false)
+        binding = FragmentFirstLandingBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/SecondLandingFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/fragmentlanding/SecondLandingFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.primetech.onefinance.R
+import com.primetech.onefinance.databinding.FragmentSecondLandingBinding
 
 class SecondLandingFragment : Fragment() {
+
+    private lateinit var binding : FragmentSecondLandingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_landing, container, false)
+        binding = FragmentSecondLandingBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
