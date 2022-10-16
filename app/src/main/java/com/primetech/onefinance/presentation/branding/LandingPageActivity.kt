e280ef76657d1701c53c561417f100033d1d7c27
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-16 18:53:13 +0700

    set all fragment landing to LandingPageActivity.kt

--- app/src/main/java/com/primetech/onefinance/presentation/branding/LandingPageActivity.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/branding/LandingPageActivity.kt
@@ -3,8 +3,13 @@ package com.primetech.onefinance.presentation.branding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
 import com.primetech.onefinance.R
 import com.primetech.onefinance.databinding.ActivityLandingPageBinding
+import com.primetech.onefinance.presentation.branding.adapter.ViewPagerAdapter
+import com.primetech.onefinance.presentation.branding.fragmentlanding.FirstLandingFragment
+import com.primetech.onefinance.presentation.branding.fragmentlanding.SecondLandingFragment
+import com.primetech.onefinance.presentation.branding.fragmentlanding.ThirdLandingFragment
 
 class LandingPageActivity : AppCompatActivity() {
 
@@ -16,9 +21,21 @@ class LandingPageActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.landing_page_animation)
-
         binding.ivLogoLandingUp.startAnimation(logoAnimation)
         binding.ivLogoLandingBot.startAnimation(logoAnimation)
 
+        setFragment()
+    }
+
+    private fun setFragment () {
+        val fragment : ArrayList<Fragment> = arrayListOf(
+            FirstLandingFragment(),
+            SecondLandingFragment(),
+            ThirdLandingFragment()
+        )
+        ViewPagerAdapter(fragment,this).also {
+            binding.viewPager.adapter = it
+        }
+        binding.circleIndicator.setViewPager(binding.viewPager)
     }
 }
