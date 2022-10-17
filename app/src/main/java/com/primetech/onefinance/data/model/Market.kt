5aa325050970e2542c7164364b71675f5e20e437
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-18 00:11:59 +0700

    Add data model Market.kt

---
+++ app/src/main/java/com/primetech/onefinance/data/model/Market.kt
@@ -0,0 +1,8 @@
+package com.primetech.onefinance.data.model
+
+data class Market(
+    val symbols : Int?,
+    val coinName : String?,
+    val amountCoin: String?,
+    val amountInUsd: String?
+)
