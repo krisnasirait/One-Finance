a025a3ca9ef77d9cb47d7ba09807e986745cdfe9
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-17 19:30:46 +0700

    Add SignUpActivity.kt and register to manifest

---
+++ app/src/main/java/com/primetech/onefinance/presentation/auth/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.primetech.onefinance.presentation.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.onefinance.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
