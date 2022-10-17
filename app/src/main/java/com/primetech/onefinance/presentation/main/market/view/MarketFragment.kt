557c062f37bb5965d52456d676ffb3039ab17597
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-17 23:49:01 +0700

    Add edtSearch and Recyclerview in layout market

--- app/src/main/java/com/primetech/onefinance/presentation/main/market/view/MarketFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/main/market/view/MarketFragment.kt
@@ -5,17 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.primetech.onefinance.R
-
+import com.primetech.onefinance.databinding.FragmentMarketBinding
 
 class MarketFragment : Fragment() {
+
+    private lateinit var binding : FragmentMarketBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market, container, false)
+        binding = FragmentMarketBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
