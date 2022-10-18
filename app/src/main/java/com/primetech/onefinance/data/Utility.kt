1eb358f9d47505e20c815e4a0e4a94ff901f9e35
Krisna Sirait <krisnapande00@outlook.com>
2022-10-18 21:38:09 +0700

    added val symbols in Porto and TradeHistory.kt

--- app/src/main/java/com/primetech/onefinance/data/Utility.kt
+++ app/src/main/java/com/primetech/onefinance/data/Utility.kt
@@ -2,6 +2,7 @@ package com.primetech.onefinance.data
 
 import com.primetech.onefinance.R
 import com.primetech.onefinance.data.model.Market
+import com.primetech.onefinance.presentation.main.wallet.portofolio.Portofolio
 
 object Utility {
 
@@ -33,6 +34,10 @@ object Utility {
         Market(R.drawable.bitcoin,"Bitcoin","1.34", "$ 40,623"),
         Market(R.drawable.ethereum,"Ethereum","4.98", "$ 89,234"),
         Market(R.drawable.tron,"TRON","0.89","$ 23,338"),
+    )
+
+    val dataPorto = arrayListOf(
+        Portofolio(R.drawable.bitcoin, "Bitcoin")
     )
 
 }
