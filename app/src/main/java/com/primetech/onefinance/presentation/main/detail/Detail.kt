29282cf3675520e8089435192ffd0766b2f1d1f3
Krisna Sirait <krisnapande00@outlook.com>
2022-10-23 20:27:06 +0700

    change from fragment to activity, and added Detail function

---
+++ app/src/main/java/com/primetech/onefinance/presentation/main/detail/Detail.kt
@@ -0,0 +1,13 @@
+package com.primetech.onefinance.presentation.main.detail
+
+class Detail(
+    val marketPair: String?,
+    val percentChange1h: Float?,
+    val percentChange24h: Float?,
+    val percentChange7d: Float?,
+    val percentChange30d: Float?,
+    val price: Float?,
+    val volume24h: Float?,
+    val circSupply: Float?,
+    val totalSupply: Float?,
+)
