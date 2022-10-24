8152f7dd61bdb579805c9518bbade1a05e199741
Krisna Sirait <krisnapande00@outlook.com>
2022-10-24 19:38:17 +0700

    added view binding for DetailActivity.kt

--- app/src/main/java/com/primetech/onefinance/presentation/main/detail/DetailActivity.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/main/detail/DetailActivity.kt
@@ -3,10 +3,15 @@ package com.primetech.onefinance.presentation.main.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.primetech.onefinance.R
+import com.primetech.onefinance.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
