eb216789715d0f12924b1cba06371a2096d8ba44
Krisna Sirait <krisnapande00@outlook.com>
2022-10-27 21:20:49 +0700

    changes error on utility data

--- app/src/main/java/com/primetech/onefinance/data/Utility.kt
+++ app/src/main/java/com/primetech/onefinance/data/Utility.kt
@@ -61,18 +61,4 @@ object Utility {
         TradeHistory(R.drawable.bitcoin,2.9,1000, "$ 40,623")
     )
 
-    val dataPorto = arrayListOf(
-        Portofolio(R.drawable.bitcoin, "Bitcoin", "1.34", "$ 50,623"),
-        Portofolio(R.drawable.ethereum,"Ethereum","4.98", "$ 89,234"),
-        Portofolio(R.drawable.tron,"TRON","0.89","$ 23,338"),
-        Portofolio(R.drawable.bitcoin,"Bitcoin","1.34", "$ 40,623")
-    )
-
-    val dataTradeHistory = arrayListOf(
-        TradeHistory(R.drawable.bitcoin, 1.3, 2500, "$ 50,623"),
-        TradeHistory(R.drawable.ethereum,1.25,36000, "$ 89,234"),
-        TradeHistory(R.drawable.tron,1.42,44000,"$ 23,338"),
-        TradeHistory(R.drawable.bitcoin,2.9,1000, "$ 40,623")
-    )
-
 }
