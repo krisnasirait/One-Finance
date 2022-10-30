055e389398967749afb22567d5e71818b4801525
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-31 05:46:35 +0700

    Make layout fragment_home.xml

--- app/src/main/java/com/primetech/onefinance/presentation/main/home/view/HomeFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/main/home/view/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.primetech.onefinance.presentation.main.home
+package com.primetech.onefinance.presentation.main.home.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
