66dcb26e05f4297e4287cf727c06f8fd674d9f6a
Affan Zanuar <affanzanuar1@gmail.com>
2022-11-01 21:53:44 +0700

    Add data model Trending.kt

---
+++ app/src/main/java/com/primetech/onefinance/presentation/main/home/adapter/TopHomeAdapter.kt
@@ -0,0 +1,25 @@
+package com.primetech.onefinance.presentation.main.home.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.primetech.onefinance.databinding.ItemTopHomeBinding
+
+class TopHomeAdapter : RecyclerView.Adapter<TopHomeAdapter.TopHomeViewHolder>() {
+
+    inner class TopHomeViewHolder (val binding : ItemTopHomeBinding) :
+        RecyclerView.ViewHolder(binding.root)  {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopHomeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TopHomeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
