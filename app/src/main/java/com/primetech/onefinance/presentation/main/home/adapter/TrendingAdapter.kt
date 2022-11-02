fdcb2d1f6bb4638e63bb5f391579bcd31fbad58b
Affan Zanuar <affanzanuar1@gmail.com>
2022-11-02 14:48:45 +0700

    Add TrendingAdapter.kt

--- app/src/main/java/com/primetech/onefinance/presentation/main/home/adapter/TrendingAdapter.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/main/home/adapter/TrendingAdapter.kt
@@ -1,8 +1,10 @@
 package com.primetech.onefinance.presentation.main.home.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.primetech.onefinance.data.model.Trending
 import com.primetech.onefinance.databinding.ItemTrendingBinding
 
@@ -14,7 +16,16 @@ class TrendingAdapter : RecyclerView.Adapter<TrendingAdapter.TrendingViewHolder>
         RecyclerView.ViewHolder (binding.root) {
 
             fun bind(item : Trending){
+                Glide.with(binding.root)
+                    .load(item.symbols)
+                    .into(binding.ivLogo)
 
+                binding.tvTitle.text = item.coinName
+                binding.tvAcroynim.text = item.acronym
+                binding.tv1h.text = item.change1Hour
+                binding.tv24h.text = item.change24Hour
+                binding.tv7d.text = item.change7Days
+                binding.tvPrice.text = item.price
             }
 
     }
@@ -27,10 +38,20 @@ class TrendingAdapter : RecyclerView.Adapter<TrendingAdapter.TrendingViewHolder>
     }
 
     override fun onBindViewHolder(holder: TrendingViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(itemTrending[position])
     }
 
     override fun getItemCount(): Int {
         return itemTrending.size
+    }
+
+    private fun clearData (){
+        itemTrending.clear()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setData (item : List<Trending>){
+        itemTrending.addAll(item)
+        notifyDataSetChanged()
     }
 }
