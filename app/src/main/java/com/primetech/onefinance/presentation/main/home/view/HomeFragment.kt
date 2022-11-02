77d8b551e19adeea602438e06d5ce684121e9cf8
Affan Zanuar <affanzanuar1@gmail.com>
2022-11-02 11:56:59 +0700

    Set Up adapter top home and register home to botnav.

--- app/src/main/java/com/primetech/onefinance/presentation/main/home/view/HomeFragment.kt
+++ app/src/main/java/com/primetech/onefinance/presentation/main/home/view/HomeFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.primetech.onefinance.data.Utility
 import com.primetech.onefinance.databinding.FragmentHomeBinding
+import com.primetech.onefinance.presentation.main.home.adapter.TopHomeAdapter
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
+
+    private lateinit var adapter : TopHomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,19 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setAdapter()
+        adapter.clearData()
+        adapter.setData(Utility.topHome)
+    }
+
+    private fun setAdapter(){
+        adapter = TopHomeAdapter()
+        binding.rvTopHome.adapter = adapter
+        binding.rvTopHome.layoutManager = GridLayoutManager(context,3)
     }
 
 }
